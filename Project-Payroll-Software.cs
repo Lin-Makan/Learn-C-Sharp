8d422d70b41544589ebb9de97e8884bb1842a3b9
Lindo Makanya <89028461+Lin-Makan@users.noreply.github.com>
2022-04-05 13:31:22 +0200

    Add files via upload

---
+++ Project-Payroll-Software.cs
@@ -0,0 +1,34 @@
+﻿// How we will create our Complete Console Application :
+// This 'Application' will generate the 'salary slips' of a 'small company'.
+
+         /// OverView   //////
+/*
+     The Staff Class
+    * Fields
+    * Properties
+    * Constructor
+    * Method
+      
+    The Manager : Staff Class
+    * Fields
+    * Properties
+    * Constructor
+    * Method
+    
+    The Admin : Staff Class
+    * Fields
+    * Property
+    * Constructor
+    * Method
+   
+    The FileReader Class
+    The PaySlip Class
+    * Fields
+    * Enum
+    * Constructor
+    * Methods
+    * The Program Class
+    * The Main () Method
+ */
+
+     
